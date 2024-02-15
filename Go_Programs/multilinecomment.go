3f3b1c285a578d535af902b4daaef69c0ab67887
Abhijeet Singh Thakur <abhiyayo@gmail.com>
2024-02-15 16:35:23 +0530

    Go Multi-line Comments
    To write multi line comments in Golang, enclose the comment lines in /* comment(s)*/.

---
+++ Go_Programs/multilinecomment.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*This is a multi-line comment.
+	This is another multi-line comment
+	This is another multi-line comment.
+	*/
+	fmt.Println("Hello World This Is Abhijeet!!!")
+}
