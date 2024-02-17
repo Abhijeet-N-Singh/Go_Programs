ff8c54706acdd1075a0ec13c24e4ee6abead447c
Abhijeet Singh Thakur <abhiyayo@gmail.com>
2024-02-17 17:21:37 +0530

    ifelsestatement.go
    Go If Else Statement is used to conditionally execute either of the two code blocks , based on the result of a condition or boolean expresssion.
    
    the syntax of Go If-Else statement is
    
    if condition {
        //code
    } else {
        //code
    }
    
    Where the condition is a boolean expression or would return a boolean value.
    
    If the condition evaluates to true, the code inside if-block executes.

---
+++ Go_Programs/ifelsestatement.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 5
+	var b int = 20
+
+	if a < b {
+		fmt.Println("a is less than b")
+	} else {
+		fmt.Println("a is not less than b")
+	}
+}
