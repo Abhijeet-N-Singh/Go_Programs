9a48d435d40bbe43c73fa962786991c06064dcfc
Abhijeet Singh Thakur <abhiyayo@gmail.com>
2024-02-19 13:58:34 +0530

    nestedbreak.go
    we write a nested for loop, and use break statement to break inner loop.
    
    break statement can break only the loop statement which is immediate to it.

---
+++ Go_Programs/nestedbreak.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int = 5
+	var i int = 0
+	for i < n {
+		var k int = 0
+		for k < n {
+			fmt.Println("* ")
+			if i == k {
+
+			}
+			k = k + 1
+		}
+		fmt.Println()
+		i = i + 1
+	}
+}
