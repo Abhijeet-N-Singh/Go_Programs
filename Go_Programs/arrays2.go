f601b2180e9c184fdd844a318fc05857e84805fe
Abhijeet Singh Thakur <abhiyayo@gmail.com>
2024-02-25 20:11:56 +0530

    arrays2.go
    Accessing Values using Index:
    
    We can access values of an array at a specific index using array-index notation.

---
+++ Go_Programs/arrays2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	primes := [5]int{2, 3, 5, 7, 11}
+	fmt.Println("Value at index 0 is : ", primes[0])
+	fmt.Println("Value at index 0 is : ", primes[1])
+	fmt.Println("Value at index 0 is : ", primes[2])
+	fmt.Println("Value at index 0 is : ", primes[3])
+	fmt.Println("Value at index 0 is : ", primes[4])
+}
