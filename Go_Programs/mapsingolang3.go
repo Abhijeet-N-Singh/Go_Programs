168f1d2ebb21ee40482dffdc2944e5963fba0f3f
Abhijeet Singh Thakur <abhiyayo@gmail.com>
2024-03-05 17:51:20 +0530

    mapsingolang3.go
    Accessing Key : Value Pairs :
    
    We can get value using key from a Map.
    
    Also , key can be used as an index notation to access the corresponding value.

---
+++ Go_Programs/mapsingolang3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var colorMap = map[string]string{"white":"#FFFFFF, "black": "#00000", "red": "#FF0000",
+"blue": "#0000FF", "green": "#00FF00"}
+
+	fmt.Println(colorMap["white"])
+	fmt.Println(colorMap["red"])
+}
