2086894779b9cd2cdf57233f1f92c2a1d5b5df74
Abhijeet Singh Thakur <abhiyayo@gmail.com>
2024-03-05 17:12:07 +0530

    mapsingolang1.go
    Initialization of Map :
    
    To initialize a Map, you can use assignment operator and a set of all key value pairs.
    
    The syntax of Map initialization is as follows :
    
    var map_name= map[key_data_type]value_data_type {key1:value1, key2:value2}
    
    in the program you can also add a key:value pair to the Map using key as index and value as element.

---
+++ Go_Programs/mapsingolang1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var colorMap = map[string]string{"white": "#FFFFFF",
+		"black": "#0000000", "red": "#FF0000", "blue": "#0000FF", "green": "#00FF00"}
+	fmt.Println(colorMap)
+}
