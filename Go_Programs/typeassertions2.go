48671b1048d2eeb21a0901aa6266ea757b252d29
Abhijeet Singh Thakur <abhiyayo@gmail.com>
2024-03-15 16:22:42 +0530

    typeassertions2.go
    Avoiding panic in Type Assertions in Golang :
    
    In Golang programming , the type assertions statement actually returns a boolean value along with the interface value.
    
    Example Statement :
    
    var a interface { }
    
    a = 12
    interfaceValue := a.(int)
    
    Here, the data type of value 12 matches with the specified type (int) , so this code assigns the value of a to interfaceValue.
    
    Along with this , a.(int) also returns a boolean value which we can store in another variable.
    
    Example Program :
    
    package main
    
    import "fmt"
    
    func main() {
    
    	// create an empty interface
    	var a interface{}
    	a = 12
    
    	//type assertion
    	interfaceValue, booleanValue := a.(int)
    
    	fmt.Println("Interface Value:", interfaceValue)
    	fmt.Println("Boolean Value:", booleanValue)
    }

---
+++ Go_Programs/typeassertions2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// create an empty interface
+	var a interface{}
+	a = 12
+
+	//type assertion
+	interfaceValue, booleanValue := a.(int)
+
+	fmt.Println("Interface Value:", interfaceValue)
+	fmt.Println("Boolean Value:", booleanValue)
+}
